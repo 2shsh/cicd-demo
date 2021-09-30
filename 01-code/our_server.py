9b9002c0a4db8eeaf39ea5e1c7a96fc7c7face86
Rushan Kashshapov <r.kashshapov@tinkoff.ru>
2021-09-30 16:34:08 +0500

    fix

--- 01-code/our_server.py
+++ 01-code/our_server.py
@@ -2,10 +2,8 @@ from flask import Flask, abort
 from prometheus_flask_exporter import PrometheusMetrics
 from random import randint
 
-
 app = Flask(__name__)
 PrometheusMetrics(app)
-
 
 @app.route("/")
 def hello_world():
@@ -13,7 +11,6 @@ def hello_world():
         return "<h1>Hello, World!</h1>"
     else:
         abort(404)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
